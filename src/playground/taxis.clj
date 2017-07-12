802d5988d306e98ba70589b25e0f081b9bc8aa63
Wenmin <wenmin@Wenmins-Mac-mini.local>
2017-07-12 15:22:10 +1000

    mod taxis

--- src/playground/taxis.clj
+++ src/playground/taxis.clj
@@ -1,11 +1,11 @@
 (ns playground.taxis
   (:require [gocatch.subsystems.utils :refer [distance-between ignore-errors]]))
 
-
 (defn make-taxi [name]
   (atom {:name name
          :lat  0
          :lng 0}))
+
 (defn move-taxi [taxi delta-lat delta-lng]
   (-> taxi
       (update :lng + delta-lng)
@@ -18,9 +18,11 @@
      (Thread/sleep 2000)
      (swap! taxi move-taxi (rand) (rand)))))
 
-
 (def taxis (atom {"A" (make-taxi "A")
                   "B" (make-taxi "B")}))
+
+(defn get-taxi [name]
+  (@taxis name))
 
 ;; e.g.
 ;; (move-in-background  (@taxis "B") 60)
@@ -30,22 +32,24 @@
   (ignore-errors (Double/parseDouble s)))
 
 (defn get-taxi-detail [taxi-name]
-  (deref (@taxis taxi-name))
-  )
+  (deref (get-taxi taxi-name)))
 
-(defn get-nearest-taxis-from [taxis]
+(defn- get-nearest-taxis-from [taxis]
   (fn [lat lng distance]
     (let [distance-to (partial distance-between [lat lng])
           taxis (vals (deref taxis))
           selected (filter (fn [taxi]
                              (> distance (distance-to [(@taxi :lat ) (@taxi :lng)])))
                            taxis)]
-      (map deref  selected))))
+      (map deref selected))))
 
+(def
+  ^{:arglists '([lat lng distance])}
+  get-taxis
+ "Public access method to get list of taxis near location to specified distance"
+  (get-nearest-taxis-from taxis))
 
-(def get-taxis (get-nearest-taxis-from taxis))
-(defn update-taxi [taxi-name lat lng]
-  (let [taxi (@taxis taxi-name)]
-    (if (nil? taxi)
-    taxi
-    (swap! taxi move-taxi lat lng))))
+(defn update-taxi! [taxi-name lat lng]
+  (if-let [taxi (@taxis taxi-name)]
+    (swap! taxi move-taxi lat lng)
+    nil))
